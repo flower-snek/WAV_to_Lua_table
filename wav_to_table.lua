3b7c6b71cb983b9b59520ee213a2705231fcbc36
flower-snek <boomerangbro12@gmail.com>
2022-01-27 13:21:27 -0500

    oh yeah i modified this more
    oops

--- wav_to_table.lua
+++ wav_to_table.lua
@@ -15,21 +15,28 @@
 
 local wav_file_name = "hold you back.wav" -- the name of the wav file
 
-local output_count = 35 -- the number of frequencies you want per unit of time(i.e. i made this for an audio visualizer: 40 means i would be able to make a visualizer with 40 bars)
-						-- currently maxes out at 1024, which should be more than enough, if you need more for some reason go to a line i'll mark with a bunch of !!!!!!!! and change the 1024 to a larger power of two
-local trim_before = 0 -- these two variables are for if you want to cut off some bars from the low or high ends of the table
-local trim_after  = 0 -- Example: output_count = 50, trim_before = 15, trim_after = 5, there will be 30 values from what would have been value 15 to 45
-local samples_per_second = 15 -- the number of samples per second (duh)
+local output_count = 200 -- the number of frequencies you want per unit of time (i.e. i made this for an audio visualizer: 40 means i would be able to make a visualizer with 40 bars)
+local trim_before = 100 -- these two variables are for if you want to cut off some bars from the low or high ends of the table
+local trim_after  = 10 -- Example: output_count = 50, trim_before = 15, trim_after = 5, there will be 30 values from what would have been value 15 to 45
+local samples_per_second = 20 -- the number of samples per second (duh)
+
+local bias = 1.1 -- this value changes how logarithmic the scale is. between 1.05 and 1.2 is probably ideal, but you start getting repeated samples at values greater than 1.15-ish. 1 is linear.
 
 local output_file = "output.lua" -- the output of the program. will just be a table.
 
+local omit_repeats = true -- this will get rid of repeated values. however, it'll make it so that the number of outputs is not what you put in above. also it'll make trimming weird.
+
 ----------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------------------------------------------
+
+function map(input, i1, i2, o1, o2)
+	slope = (o2 - o1) / (i2 - i1)
+	return o1 + slope * (input - i1)
+end
 
 dofile("wav.lua")
-
 
 -- Read audio file
 local reader = wav.create_context(wav_file_name, "r")
@@ -61,25 +68,37 @@ local output = io.open(output_file, "w")
 io.output(output)
 io.write("{")
 
-for ms=0, song_length_ms, ms_spacing do
-	io.write(string.format("{%.4f,", ms/1000))
-	reader.set_position(math.floor(reader.get_sample_from_ms(math.floor(ms)))) -- double math.floor because thanks wav
-	local samples = reader.get_samples(1024)[1] -- !!!!!!!!!!!!!!!!!!!!
-	for i=1, samples.n do
-		samples[i] = samples[i] / 32768
-	end
-	local analyzer = wav.create_frequency_analyzer(samples, reader.get_sample_rate())
-	local num_frequencies = #analyzer.get_frequencies()
-	local splice = num_frequencies / (output_count-1)
-	for i, frequency in ipairs(analyzer.get_frequencies()) do
-		if i/splice >= trim_before and i/splice <= (output_count-trim_after) and i % splice <= 1 then
-			io.write(string.format("%.4f,", frequency.weight))
-			--print(string.format("%.2f: %f", frequency.freq, frequency.weight))
+for ms=0, song_length_ms - ms_spacing, ms_spacing do
+	if math.floor(ms) < reader.get_samples_per_channel() then
+		io.write(string.format("{%.4f,", ms/1000))
+		reader.set_position(math.floor(reader.get_sample_from_ms(math.floor(ms)))) -- double math.floor because thanks wav
+		local samples = reader.get_samples(16384)[1] -- if you're using a low bias value (1.1 or less) feel free to lower this number to like 1024 to make things faster
+		for i=1, samples.n do
+			samples[i] = samples[i] / 32768
 		end
+		local analyzer = wav.create_frequency_analyzer(samples, reader.get_sample_rate())
+		local frequencies = analyzer.get_frequencies()
+		local num_frequencies = #frequencies
+		for i=trim_before + 1, output_count - trim_after do
+			-- map this value from 0, output_count to 0, num_frequencies
+			local freqnum = math.floor(map(bias^i, 1, bias^output_count, 1, num_frequencies+1))
+			if ((omit_repeats and freqnum ~= math.floor(map(bias^(i+1), 1, bias^output_count, 1, num_frequencies+1))) or not omit_repeats) and frequencies[freqnum] then
+				--print(i .. ' ' .. freqnum .. ' ' .. frequencies[freqnum].freq)
+				io.write(string.format("%.4f,", (frequencies[freqnum].weight)))
+			end
+		end
+		--[[
+		local splice = math.log(num_frequencies) / (output_count-1)
+		for i, frequency in ipairs(analyzer.get_frequencies()) do
+			if i/splice >= trim_before and i/splice <= (output_count-trim_after) and i % splice <= 1 then
+				io.write(string.format("%.4f,", frequency.weight))
+				--print(string.format("%.2f: %f", frequency.freq, frequency.weight))
+			end
+		end
+		]]
+		io.write("},\n")
+		print(string.format("%.2f", 100*ms/song_length_ms).."% done")
 	end
-	
-	io.write("},\n")
-	print(string.format("%.2f", 100*ms/song_length_ms).."% done")
 end
 io.write("}")
 io.close(output)
